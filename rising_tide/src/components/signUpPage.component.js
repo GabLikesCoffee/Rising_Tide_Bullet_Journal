5da305149256f1b886919b030dc251b6069b6343
Garth Simpson <66145789+Tallinax@users.noreply.github.com>
2022-10-24 17:20:41 -0400

    Takes in user information (not sent to server)

--- rising_tide/src/components/signUpPage.component.js
+++ rising_tide/src/components/signUpPage.component.js
@@ -1,12 +1,80 @@
 import React, { Component } from 'react';
 
 export default class SignUp extends Component {
+
+    constructor(props){
+		super(props);
+        
+        this.changeUsername= this.changeUsername.bind(this);
+        this.changePassword= this.changePassword.bind(this);
+        
+        this.onSubmit=this.onSubmit.bind(this);
+		this.state={
+			_id:"",//Username
+			password:"",
+			
+
+			habit:[{
+				activity:"",
+				isCompleted: false,
+				completionDate:{
+					day:0,
+					month:"",
+					year:0,
+				}
+			}],
+			
+			Journal:[{
+				date:{
+					day:0,
+					month:"",
+					year:0,
+				},
+				
+				freeResponse:"",
+				
+				mood:[false]
+				
+				
+			}]
+			
+			
+			
+		
+
+		}
+	}
     
-    onSubmit(e) {
-        e.preventDefault();
-        //Placeholder text change when form is submitted
-        document.getElementById("signup").innerHTML = "Welcome, new user!";
+	changeUsername = (e)=>{
+		this.setState({
+			username: e.target.value
+		});
+	}
+	changePassword = (e)=>{
+		this.setState({
+			password: e.target.value
+		});
+	}
+	
+	 
+
+    onSubmit= (e)=>{
+        
+        const user={
+			_id: this.state.username,
+			password: this.state.password,
+			
+		}
+		e.preventDefault();
+	
+        
+        
+        console.log(user)
+        document.getElementById("signup").innerHTML = "Welcome, new user!"
+        
+		
     }
+
 
     render() {
         return(
@@ -15,11 +83,21 @@ export default class SignUp extends Component {
                     <form onSubmit={this.onSubmit}>
                         <h2 id="signup">Sign Up Here!</h2>
                         <br/>
-                        <input type="textbox"  className="form-control" placeholder="Username" required></input>
+                        <input type="textbox"  className="form-control" placeholder="Username" 
+                        required 
+                        value={this.state.username}
+                        onChange={this.changeUsername}>
 
-                        <input type="password"  className="form-control" placeholder="Password" required></input>
+                        </input>
 
-                        <button type="submit" id="signUpBtn" className="btn btn-primary btn-block">Sign Up</button>
+                        <input type="password"  className="form-control" placeholder="Password" 
+                        required
+                        value={this.state.password}
+                        onChange={this.changePassword}>
+
+                        </input>
+
+                        <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
                     </form>
                 </center>
             </div>

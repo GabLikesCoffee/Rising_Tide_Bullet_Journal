be49e1c6296ffdd9a35bcd1bd96339a6b03d6f89
Garth Simpson <66145789+Tallinax@users.noreply.github.com>
2022-10-24 20:29:12 -0400

    Takes in user information (not sent to server)

--- rising_tide/src/components/signInPage.component.js
+++ rising_tide/src/components/signInPage.component.js
@@ -1,27 +1,109 @@
-import React, { Component } from 'react';
+import React, { Component }from 'react';
+
 
 export default class SignIn extends Component {
+    
+	
+    constructor(props){
+		super(props);
+        
+        this.changeUsername= this.changeUsername.bind(this);
+        this.changePassword= this.changePassword.bind(this);
+        
+        this.onSubmit=this.onSubmit.bind(this);
+		this.state={
+			_id:"",//Username
+			password:"",
+			
 
-    onSubmit(e) {
-        e.preventDefault();
-        //Placeholder text change when form is submitted
+			habit:[{
+				activity:"",
+				isCompleted: false,
+				completionDate:{
+					day:0,
+					month:"",
+					year:0,
+				}
+			}],
+			
+			Journal:[{
+				date:{
+					day:0,
+					month:"",
+					year:0,
+				},
+				
+				freeResponse:"",
+				
+				mood:[false]
+				
+				
+			}]
+			
+			
+			
+		
+
+		}
+	}
+    
+	changeUsername = (e)=>{
+		this.setState({
+			username: e.target.value
+		});
+	}
+	changePassword = (e)=>{
+		this.setState({
+			password: e.target.value
+		});
+	}
+	
+	 
+
+    onSubmit= (e)=>{
+        
+        const user={
+			_id: this.state.username,
+			password: this.state.password,
+			
+		}
+		e.preventDefault();
+	
+        
+        
+        console.log(user)
         document.getElementById("p").innerHTML = "Logged in!"
+        
+		
     }
 
+    
     render() {
-
         return(
             <div className="container" id="containerSignUpOrOut">
                 <center>
                     <form onSubmit={this.onSubmit}>
-                    <h2 id="p">Log in Here!</h2>
-                    <br/>
-                    <input type="textbox" className="form-control" placeholder="Username" required></input>
-                    <input type="password" className="form-control" placeholder="Password" required></input>
-                    <button type="submit" id="signInBtn" className="btn btn-primary btn-block">Log in</button>
+                        <h2 id="p">Log in Here!</h2>
+                        <br/>
+                        <input type="textbox"  className="form-control" placeholder="Username" 
+                        required 
+                        value={this.state.username}
+                        onChange={this.changeUsername}>
+
+                        </input>
+
+                        <input type="password"  className="form-control" placeholder="Password" 
+                        required
+                        value={this.state.password}
+                        onChange={this.changePassword}>
+
+                        </input>
+
+                        <button type="submit" className="btn btn-primary btn-block">Sign In</button>
                     </form>
                 </center>
             </div>
+
         )
     }
 }

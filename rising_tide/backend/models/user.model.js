54b21a398c0842a0899f8679862f13c3767a4f08
Garth Simpson <66145789+Tallinax@users.noreply.github.com>
2022-10-26 18:18:23 -0400

    nested user model with journal and habit lists

--- rising_tide/backend/models/user.model.js
+++ rising_tide/backend/models/user.model.js
@@ -1,13 +1,17 @@
 const mongoose= require('mongoose')
+import {journal} from './journal.model.js'
+import {habit} from './habit.model.js'
 
 const userSchema= mongoose.Schema({
     _id: { type: String, required: true, unique: true, minlength: 3},
     password: {type: String, required: true},
-    email:{ type: String, required: true}
+    email:{ type: String, required: true},
+    habitList:{type:[habit]},
+    journalList:{type:[journal]}
     
 },{
     timestamps:true,
 });
 
-const user=mongoose.model("user", euserSchema);
+const user=mongoose.model("user", userSchema);
 module.exports= user;

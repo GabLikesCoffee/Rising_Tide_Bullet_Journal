0c853419bd4d7cb7864b5cd97bc367a0fda0dbcd
gooigii <luigibarbero@gmail.com>
2022-11-05 12:17:06 -0400

    routes re-added
    Need to work on the actual code next week.

--- rising_tide/backend/server.js
+++ rising_tide/backend/server.js
@@ -23,6 +23,12 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
+const journalsRouter = require("./routes/journals");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", journalsRouter);
+app.use("/users", usersRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });

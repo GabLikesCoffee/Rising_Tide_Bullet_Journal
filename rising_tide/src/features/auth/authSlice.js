fba3c9c599c8d52ab39298f6110d6d65a6c54dd3
Garth Simpson <66145789+Tallinax@users.noreply.github.com>
2022-11-06 03:08:00 -0500

    start of JWT authentication

---
+++ rising_tide/src/features/auth/authSlice.js
@@ -0,0 +1,61 @@
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
+import authService from "./authService"
+
+
+const user = JSON.parse(localStorage.getItem("user"))
+
+const initialState={
+    user: user ? user:null,
+    errorDetected: false,
+    success: false,
+    isLoading: false,
+    message:""
+}
+
+export const register = createAsyncThunk("auth/register", 
+    async (user, thunkAPI)=>{
+        try{
+            return await authService.register(user)
+        }
+        catch (error){
+            const messageError= (error.response && error.response.data.message)|| error.toString()
+            return thunkAPI.rejectWithValue(messageError)
+        }
+    })
+
+export const authSlice= createSlice({
+    
+    name: "auth",
+    initialState,
+    reducers:{
+        reset: (state)=>{
+            state.isLoading=false
+            state.success=false
+            state.errorDetected=false
+            state.message=""
+            
+        }
+    },
+    extraReducers:(builder)=>{
+        builder
+            .addCase(register.pending,(state)=>{
+                state.isLoading=true
+            })
+            .addCase(register.fulfilled,(state,action)=>{
+                state.isLoading=false
+                state.success=true
+                state.user= action.payload
+            })
+            .addCase(register.rejected,(state,action)=>{
+                state.isLoading=false
+                state.errorDetected=true
+                state.message= action.payload
+                state.user=null
+            })
+    }
+})
+
+
+
+export const {reset} = authSlice.actions
+export default authSlice.reducer

f1559edbfdbbba321d190191e4e63a442c552685
Garth Simpson <66145789+Tallinax@users.noreply.github.com>
2022-11-06 02:59:56 -0500

    Removing nested models which caused an error

--- rising_tide/backend/models/user.model.js
+++ rising_tide/backend/models/user.model.js
@@ -1,13 +1,9 @@
 const mongoose= require('mongoose')
-import {journal} from './journal.model.js'
-import {habit} from './habit.model.js'
 
 const userSchema= mongoose.Schema({
-    _id: { type: String, required: true, unique: true, minlength: 3},
+    username: { type: String, required: true, unique: true, minlength: 3},
     password: {type: String, required: true},
     email:{ type: String, required: true},
-    habitList:{type:[habit]},
-    journalList:{type:[journal]}
     
 },{
     timestamps:true,

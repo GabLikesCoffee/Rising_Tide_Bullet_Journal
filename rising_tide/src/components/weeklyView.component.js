cd7f688b27b8bbf2467e08f31f081713553bf0d5
GabLikesCoffee <gabisgr8@gmail.com>
2022-11-06 10:38:01 -0500

    Updated with new css file and updated weekly view page

--- rising_tide/src/components/weeklyView.component.js
+++ rising_tide/src/components/weeklyView.component.js
@@ -1,89 +1,132 @@
 //WEEKLY VIEW
 
 import React, { Component } from 'react';
-import seashell from '../Assets/shell.PNG'
+import seashell from '../Assets/shell.PNG';
+import { Link } from 'react-router-dom';
 
+//Stores a random number for random shell rotation
 let randomNumber;
+//Stores the style string for each rotation
+let rotateStrings = [];
 
-let shell = document.getElementsByClassName("weeklyShell");
+//Allows for random rotating of the shells so they don't look so boring
+for(let i = 0; i < 7; i++){
 
-//Unfinished will allow for random rotating of the shells so they don't look so boring
-for(let i = 0; i < shell.length; i++){
-    randomNumber = Math.floor(Math.random() * (90 + 1));
-    console.log(randomNumber-45);
-    shell[i].style.transform = "rotate(90deg)";
+    //Generates random number between 0 and 70
+    randomNumber = Math.floor(Math.random() * (70 + 1));
+
+    //Subtracts 35 so the degree of rotation is between -35 and +35
+    randomNumber -= 35;
+
+    //Creates the style string for the rotation
+    rotateStrings[i] = "rotate(" + randomNumber + "deg)";
 }
+
+//Starts list of days from today on moves on from there
+//Keeps track of the day of the week
+let dayOfTheWeek = new Date().getDate();
+let weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 export default class WeeklyView extends Component {
 
     render() {
         return(
-            <div className="container" id="container">
+            <div className="container" id="weeklyViewContainer">
                 <h1>
                     <center>
                         Weekly View
                     </center>
                 </h1>
-                <table id="weeklyViewTable">
-                    <tbody>
-                        <tr>
-                            <td>
-                                Monday
-                            </td>
-                            <td class="weeklyViewTableBorder">
-                                <img class="weeklyShell" src={seashell} alt="seashell"></img>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Tuesday
-                            </td>
-                            <td class="weeklyViewTableBorder">
-                                <img class="weeklyShell" src={seashell} alt="seashell"></img>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Wednesday
-                            </td>
-                            <td class="weeklyViewTableBorder">
-                                <img class="weeklyShell" src={seashell} alt="seashell"></img>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Thursday
-                            </td>
-                            <td class="weeklyViewTableBorder">
-                                <img class="weeklyShell" src={seashell} alt="seashell"></img>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Friday
-                            </td>
-                            <td class="weeklyViewTableBorder">
-                                <img class="weeklyShell" src={seashell} alt="seashell"></img>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Saturday
-                            </td>
-                            <td class="weeklyViewTableBorder">
-                                <img class="weeklyShell" src={seashell} alt="seashell"></img>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Sunday
-                            </td>
-                            <td class="weeklyViewTableBorder">
-                                <img class="weeklyShell" src={seashell} alt="seashell"></img>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <center>
+                    <table id="weeklyViewTable">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <label for="day1Shell">
+                                        Today
+                                    </label>
+                                </td>
+                                <td class="weeklyViewTableBorder">
+                                    <Link to="/">
+                                        <img class="weeklyShell" name="day1Shell" src={seashell} alt="seashell" style={{transform: rotateStrings[0]}}></img>
+                                    </Link>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>
+                                        {weekDay[(dayOfTheWeek+1) % 7]}
+                                    </label>
+                                </td>
+                                <td class="weeklyViewTableBorder">
+                                    <Link to="/">
+                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[1]}}></img>
+                                    </Link>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>
+                                        {weekDay[(dayOfTheWeek+2) % 7]}
+                                    </label>
+                                </td>
+                                <td class="weeklyViewTableBorder">
+                                    <Link to="/">
+                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[2]}}></img>
+                                    </Link>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>
+                                        {weekDay[(dayOfTheWeek+3) % 7]}
+                                    </label>
+                                </td>
+                                <td class="weeklyViewTableBorder">
+                                    <Link to="/">
+                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[3]}}></img>
+                                    </Link>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>
+                                        {weekDay[(dayOfTheWeek+4) % 7]}
+                                    </label>
+                                </td>
+                                <td class="weeklyViewTableBorder">
+                                    <Link to="/">
+                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[4]}}></img>
+                                    </Link>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>
+                                        {weekDay[(dayOfTheWeek+5) % 7]}
+                                    </label>
+                                </td>
+                                <td class="weeklyViewTableBorder">
+                                    <Link to="/">
+                                    <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[5]}}></img>
+                                    </Link>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>
+                                        {weekDay[(dayOfTheWeek+6) % 7]}
+                                    </label>
+                                </td>
+                                <td class="weeklyViewTableBorder">
+                                    <Link to="/">
+                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[6]}}></img>
+                                    </Link>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </center>
 
             </div>
 

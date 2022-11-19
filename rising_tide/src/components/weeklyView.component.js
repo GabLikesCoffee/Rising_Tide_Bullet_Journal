a373501e11a7a7a02950d5fd9ae5a04cae5f9752
GabLikesCoffee <gabisgr8@gmail.com>
2022-11-18 19:40:43 -0500

    Created journal viewing and editing page (ugly for now). Also weeklyview calculates past days and console logs the info when a shell is clicked on

--- rising_tide/src/components/weeklyView.component.js
+++ rising_tide/src/components/weeklyView.component.js
@@ -2,12 +2,35 @@
 
 import React, { Component } from 'react';
 import seashell from '../Assets/shell.PNG';
-import { Link } from 'react-router-dom';
 
 //Stores a random number for random shell rotation
 let randomNumber;
 //Stores the style string for each rotation
 let rotateStrings = [];
+
+//Starts list of days from today on moves on from there
+//Keeps track of the day of the week
+let dayOfTheWeek = new Date().getDate();
+let weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let last7Days=[]
+
+//GLOBAL VARIABLES THAT WE FILL WITH DATABASE INFORMATION
+let mood
+let completedHabits
+let dailyAffirmation
+
+//Finds the last 7 days, turns it into an object and pushes it to an array to be used in the onEditPage functions
+for(let i = 0; i < 7; i++){
+    let date = new Date();
+    date.setDate(date.getDate() - i)
+    
+    let last7DaysObject = {
+        day: date.getDate(), 
+        month: date.getMonth() + 1,
+        year: date.getFullYear()
+    }
+    last7Days.push(last7DaysObject);
+}
 
 //Allows for random rotating of the shells so they don't look so boring
 for(let i = 0; i < 7; i++){
@@ -22,131 +45,273 @@ for(let i = 0; i < 7; i++){
     rotateStrings[i] = "rotate(" + randomNumber + "deg)";
 }
 
-//Starts list of days from today on moves on from there
-//Keeps track of the day of the week
-let dayOfTheWeek = new Date().getDate();
-let weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+//1 is for yesterday, 2 is for the day before yesterday, etc
+const onEditPage1 = event => {
+    let weeklyViewPage = document.getElementById("weeklyView");
+    let weeklyViewEditPage = document.getElementById("weeklyViewEditPage");
+    weeklyViewPage.setAttribute("hidden", true);
+    weeklyViewEditPage.removeAttribute("hidden"); 
 
+    let loadDate = last7Days[0]
+
+    //Debug purposes shows date given
+    console.log(loadDate)
+
+    //mood = find info from database TEMPORARY VALUE BELOW
+    mood = "happy"
+    //completedHabits = find info from database TEMPORARY VALUE BELOW
+    completedHabits = "Jog, Walk Dog, Take meds"
+    //dailyAffirmation = find info from database TEMPORARY VALUE BELOW
+    dailyAffirmation = "Today was a good day."
+
+    loadJournalEditPage();
+}
+
+const onEditPage2 = event => {
+    let weeklyViewPage = document.getElementById("weeklyView");
+    let weeklyViewEditPage = document.getElementById("weeklyViewEditPage");
+    weeklyViewPage.setAttribute("hidden", true);
+    weeklyViewEditPage.removeAttribute("hidden"); 
+
+    let loadDate = last7Days[1]
+
+    //Debug purposes shows date given
+    console.log(loadDate)
+
+    //mood = find info from database TEMPORARY VALUE BELOW
+    mood = "happy"
+    //completedHabits = find info from database TEMPORARY VALUE BELOW
+    completedHabits = "Jog, Walk Dog, Take meds"
+    //dailyAffirmation = find info from database TEMPORARY VALUE BELOW
+    dailyAffirmation = "Today was a good day."
+
+    loadJournalEditPage();
+}
+
+const onEditPage3 = event => {
+    let weeklyViewPage = document.getElementById("weeklyView");
+    let weeklyViewEditPage = document.getElementById("weeklyViewEditPage");
+    weeklyViewPage.setAttribute("hidden", true);
+    weeklyViewEditPage.removeAttribute("hidden"); 
+
+    let loadDate = last7Days[2]
+    console.log(loadDate)
+
+    //mood = find info from database TEMPORARY VALUE BELOW
+    mood = "happy"
+    //completedHabits = find info from database TEMPORARY VALUE BELOW
+    completedHabits = "Jog, Walk Dog, Take meds"
+    //dailyAffirmation = find info from database TEMPORARY VALUE BELOW
+    dailyAffirmation = "Today was a good day."
+
+    loadJournalEditPage();
+}
+
+const onEditPage4 = event => {
+    let weeklyViewPage = document.getElementById("weeklyView");
+    let weeklyViewEditPage = document.getElementById("weeklyViewEditPage");
+    weeklyViewPage.setAttribute("hidden", true);
+    weeklyViewEditPage.removeAttribute("hidden"); 
+
+    let loadDate = last7Days[3]
+    console.log(loadDate)
+
+    //mood = find info from database TEMPORARY VALUE BELOW
+    mood = "happy"
+    //completedHabits = find info from database TEMPORARY VALUE BELOW
+    completedHabits = "Jog, Walk Dog, Take meds"
+    //dailyAffirmation = find info from database TEMPORARY VALUE BELOW
+    dailyAffirmation = "Today was a good day."
+
+    loadJournalEditPage();
+}
+
+const onEditPage5 = event => {
+    let weeklyViewPage = document.getElementById("weeklyView");
+    let weeklyViewEditPage = document.getElementById("weeklyViewEditPage");
+    weeklyViewPage.setAttribute("hidden", true);
+    weeklyViewEditPage.removeAttribute("hidden"); 
+
+    let loadDate = last7Days[4]
+    console.log(loadDate)
+
+    //mood = find info from database TEMPORARY VALUE BELOW
+    mood = "happy"
+    //completedHabits = find info from database TEMPORARY VALUE BELOW
+    completedHabits = "Jog, Walk Dog, Take meds"
+    //dailyAffirmation = find info from database TEMPORARY VALUE BELOW
+    dailyAffirmation = "Today was a good day."
+
+    loadJournalEditPage();
+}
+
+const onEditPage6 = event => {
+    let weeklyViewPage = document.getElementById("weeklyView");
+    let weeklyViewEditPage = document.getElementById("weeklyViewEditPage");
+    weeklyViewPage.setAttribute("hidden", true);
+    weeklyViewEditPage.removeAttribute("hidden"); 
+
+    let loadDate = last7Days[5]
+    console.log(loadDate)
+
+    //mood = find info from database TEMPORARY VALUE BELOW
+    mood = "happy"
+    //completedHabits = find info from database TEMPORARY VALUE BELOW
+    completedHabits = "Jog, Walk Dog, Take meds"
+    //dailyAffirmation = find info from database TEMPORARY VALUE BELOW
+    dailyAffirmation = "Today was a good day."
+
+    loadJournalEditPage();
+}
+
+const onEditPage7 = event => {
+    let weeklyViewPage = document.getElementById("weeklyView");
+    let weeklyViewEditPage = document.getElementById("weeklyViewEditPage");
+    weeklyViewPage.setAttribute("hidden", true);
+    weeklyViewEditPage.removeAttribute("hidden"); 
+
+    let loadDate = last7Days[6]
+    console.log(loadDate)
+
+    //mood = find info from database TEMPORARY VALUE BELOW
+    mood = "happy"
+    //completedHabits = find info from database TEMPORARY VALUE BELOW
+    completedHabits = "Jog, Walk Dog, Take meds"
+    //dailyAffirmation = find info from database TEMPORARY VALUE BELOW
+    dailyAffirmation = "Today was a good day."
+
+    loadJournalEditPage();
+}
+
+let loadJournalEditPage = function(){
+    let displayMood = document.getElementById("displayMood");
+    let displayHabits = document.getElementById("displayHabits");
+    let displayTextInput = document.getElementById("displayTextInput");
+    displayMood.innerHTML = "Mood: <p>" + mood + "</p>";
+    displayHabits.innerHTML = "Completed Habits: <p>" + completedHabits + "</p>";
+    displayTextInput.innerHTML = "Daily Affirmation: <textarea>" + dailyAffirmation + "</textarea>";
+}
+
+const onBackButton = event => {
+    let weeklyViewPage = document.getElementById("weeklyView");
+    let weeklyViewEditPage = document.getElementById("weeklyViewEditPage");
+    weeklyViewEditPage.setAttribute("hidden", true);
+    weeklyViewPage.removeAttribute("hidden"); 
+    mood = "";
+    completedHabits = "";
+    dailyAffirmation = "";
+
+}
+
+const onSubmitJournalEdit = event => {
+    //Go back to weekly view page
+    let weeklyViewPage = document.getElementById("weeklyView");
+    let weeklyViewEditPage = document.getElementById("weeklyViewEditPage");
+    weeklyViewEditPage.setAttribute("hidden", true);
+    weeklyViewPage.removeAttribute("hidden"); 
+    mood = "";
+    completedHabits = "";
+    dailyAffirmation = "";
+
+    //Show submitted to confirm submission
+    document.getElementById("weeklyViewHeader").innerHTML = "Submitted!";
+}
 
 export default class WeeklyView extends Component {
-
-
-
-
-    
 
     render() {
         return(
             <div className="container" id="weeklyViewContainer">
                <center>
-                    <h1>
+                    <h1 id = "weeklyViewHeader">
                         Weekly View
                     </h1>
                 </center>
-                <center>
-                    <table id="weeklyViewTable">
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <Link to="/">
+                <div id="weeklyView">
+                    <center>
+                        <table id="weeklyViewTable">
+                            <tbody>
+                                <tr>
+                                    <td>
                                         <label htmlFor="day1Shell">
                                             Today
                                         </label>
-                                    </Link>
-                                </td>
-                                <td class="weeklyViewTableBorder">
-                                    <Link to="/">
-                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[0]}}></img>
-                                    </Link>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <Link to="/">
+                                    </td>
+                                    <td className="weeklyViewTableBorder">
+                                        <input class = "shellButton" onClick={onEditPage1} type="image" src={seashell} style={{transform: rotateStrings[0]}} alt="seashell"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
                                         <label>
                                             {weekDay[(dayOfTheWeek + 6) % 7]}
                                         </label>
-                                    </Link>
-                                </td>
-                                <td class="weeklyViewTableBorder">
-                                    <Link to="/">
-                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[1]}}></img>
-                                    </Link>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <Link to="/">
+                                    </td>
+                                    <td className="weeklyViewTableBorder">
+                                        <input class = "shellButton" onClick={onEditPage2} type="image" src={seashell} style={{transform: rotateStrings[1]}} alt="seashell"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
                                         <label>
                                             {weekDay[(dayOfTheWeek + 5) % 7]}
                                         </label>
-                                    </Link>
-                                </td>
-                                <td class="weeklyViewTableBorder">
-                                    <Link to="/">
-                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[2]}}></img>
-                                    </Link>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <Link to="/">
+                                    </td>
+                                    <td className="weeklyViewTableBorder">
+                                        <input class = "shellButton" onClick={onEditPage3} type="image" src={seashell} style={{transform: rotateStrings[2]}} alt="seashell"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
                                         <label>
                                             {weekDay[(dayOfTheWeek + 4) % 7]}
                                         </label>
-                                    </Link>
-                                </td>
-                                <td class="weeklyViewTableBorder">
-                                    <Link to="/">
-                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[3]}}></img>
-                                    </Link>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <Link to="/">
+                                    </td>
+                                    <td className="weeklyViewTableBorder">
+                                        <input class = "shellButton" onClick={onEditPage4} type="image" src={seashell} style={{transform: rotateStrings[3]}} alt="seashell"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
                                         <label>
                                             {weekDay[(dayOfTheWeek + 3) % 7]}
                                         </label>
-                                    </Link>
-                                </td>
-                                <td class="weeklyViewTableBorder">
-                                    <Link to="/">
-                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[4]}}></img>
-                                    </Link>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <Link to="/">
+                                    </td>
+                                    <td className="weeklyViewTableBorder">
+                                        <input class = "shellButton" onClick={onEditPage5} type="image" src={seashell} style={{transform: rotateStrings[4]}} alt="seashell"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
                                         <label>
                                             {weekDay[(dayOfTheWeek + 2) % 7]}
                                         </label>
-                                    </Link>
-                                </td>
-                                <td class="weeklyViewTableBorder">
-                                    <Link to="/">
-                                    <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[5]}}></img>
-                                    </Link>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <Link to="/">
+                                    </td>
+                                    <td className="weeklyViewTableBorder">
+                                        <input class = "shellButton" onClick={onEditPage6} type="image" src={seashell} style={{transform: rotateStrings[5]}} alt="seashell"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
                                         <label>
                                             {weekDay[(dayOfTheWeek + 1) % 7]}
                                         </label>
-                                    </Link>
-                                </td>
-                                <td class="weeklyViewTableBorder">
-                                    <Link to="/">
-                                        <img class="weeklyShell" src={seashell} alt="seashell" style={{transform: rotateStrings[6]}}></img>
-                                    </Link>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </center>
+                                    </td>
+                                    <td className="weeklyViewTableBorder">
+                                        <input class = "shellButton" onClick={onEditPage7} type="image" src={seashell} style={{transform: rotateStrings[6]}} alt="seashell"/>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </center>
+                </div>
+                <div id="weeklyViewEditPage" hidden>
+                <button type="button" onClick={onBackButton} id="weeklyViewBackBtn" className="btn btn-primary">Back</button>
+                    <h3 id="displayMood">Mood:</h3>
+                    <h3 id="displayHabits">Habits:</h3>
+                    <h3 id="displayTextInput">Self Reflection:</h3>
+                    <button type="button" onClick={onSubmitJournalEdit} id="weeklyViewEditJournalBtn" className="btn btn-primary">Edit Journal</button>
+                </div>
 
             </div>
 
